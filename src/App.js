92979f16bdda0c76c6775773ec5e30710db18a33
akshatsinghania <singhaniaakshat1@gmail.com>
2021-06-20 19:11:09 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
 
 const App = () => {
 	return (
 		<div>
 			<Navbar />
+			<Home />
 		</div>
 	);
 };
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HomeWrapper } from './Home.style';
+
+const Home = () => {
+	return (
+		<HomeWrapper>
+			<h1>Hello</h1>
+		</HomeWrapper>
+	);
+};
+
+export default Home;
